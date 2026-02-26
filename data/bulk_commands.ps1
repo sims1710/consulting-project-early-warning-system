a86d33b60a37da19c49685a93021d3575f190ce9
sims1710 <simritib1@gmail.com>
2026-02-27 00:31:42 +0800

    Updated agent instructions, added extra tool + mappings & added `requirement.txt`

--- data/bulk_commands.ps1
+++ data/bulk_commands.ps1
@@ -6,3 +6,6 @@ curl.exe -X POST "$ES_URL/projects_sentiment/_bulk" -H "Content-Type: applicatio
 
 # Playbooks
 curl.exe -X POST "$ES_URL/project_playbooks/_bulk" -H "Content-Type: application/x-ndjson" -H "Authorization: ApiKey $API_KEY" --data-binary "@project_playbooks.ndjson"
+
+# Live timeseries projects
+curl.exe -X POST "$ES_URL/projects_live_timeseries/_bulk" -H "Content-Type: application/x-ndjson" -H "Authorization: ApiKey $API_KEY" --data-binary "@projects_live_timeseries.ndjson"
